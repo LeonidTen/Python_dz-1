d09f1f35c7817d35cd96cbb0e55f48d885913ecc
leonid <tenleonid@gmail.com>
2023-02-06 10:42:35 +0600

    dz10

---
+++ dz10/dz10.py
@@ -0,0 +1,13 @@
+# Реализуйте алгоритм перемешивания списка.
+
+
+import random
+
+list = ['Реализуйте', 'алгоритм', 'перемешивания', 'списка']
+print('Исходный список ->    ', list)
+random.shuffle(list)
+print('Перемешаный список -> ', list)
+mass = [i for i in range(random.randint(3,6))]
+print('Исходный список ->    ', mass)
+random.shuffle(mass)
+print('Перемешаный список -> ', mass)
